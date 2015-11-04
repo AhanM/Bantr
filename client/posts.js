f09638aca7e6125a000ee82dca709219a1ab7827
Cryptic <ahan.m13@gmail.com>
2015-11-05 00:52:18 +0530

    Secure Upvoting Functionality

---
+++ client/posts.js
@@ -0,0 +1,50 @@
+Posts = new Meteor.Collection("posts");
+
+Template.home.helpers({
+    posts: function () {
+        return Posts.find({},{sort:{time:-1}});
+    }
+});
+
+Template.Post.helpers({
+    upvotedClass: function(){
+        var userId = Meteor.userId();
+        if(userId && !_.include(this.upvoters, userId)) {
+            return 'btn-primary upvoteable';
+        } else {
+            return 'disabled';
+        }
+    }
+});
+
+Template.home.events({
+    "submit form": function (event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+        // Get value from form element
+        var text = event.target.text.value;
+
+        if(text!= "" && Meteor.user()){
+            // Insert a task into the collection
+            Posts.insert({
+                text: text,
+                username: Meteor.user().username || Meteor.user().profile.name,
+                points: 0,
+                votedUp : false,
+                upvoters : [],
+                time: new Date(), // current time
+                createdAt : (new Date().toLocaleTimeString())+
+                " "+(new Date().toLocaleDateString())
+            });
+        }
+        // Clear form
+        event.target.text.value = "";
+    }
+});
+
+Template.Post.events({
+    "click .upvoteable" : function(event) {
+        event.preventDefault();
+        Meteor.call('upvote', this._id);
+    }
+});
